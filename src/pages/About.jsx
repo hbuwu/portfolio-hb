1d7e30ebb20284c35c4ffdc712b1963f2a17094b
Alex <alexander.vanbets@gmail.com>
2021-08-02 16:26:08 +0200

    Set up base for portfolio

---
+++ src/pages/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function About() {
+    return(
+        <>
+            <div className="introScreen"></div>
+            <div>
+                <p>
+                    Welcome to my portfolio!
+                </p>
+            </div>
+        </>
+    );
+}
---
+++ src/pages/Commissions.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Commissions() {
+    return (
+        <h1>dab</h1>
+    )
+}
