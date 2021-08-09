4d73e10c8c3163f0b72a263269aaaa7415df28a3
Alex <alexander.vanbets@gmail.com>
2021-08-09 12:52:28 +0200

    Add brand to intro screen

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 export default function About() {
     return(
         <>
-            <div className="introScreen"></div>
             <div>
                 <p>
                     Welcome to my portfolio!
--- src/pages/Intro.jsx
+++ src/pages/Intro.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 export default function Intro() {
-    return(
+    return (
         <>
-            <div className="introScreen"></div>
+            <div className='introScreen'>
+                <div className='introBox'>
+                    <img src='./images/logo.png' alt='Hanori Logo' className="introLogo"></img>
+                    <p className='introSubTitle'>Graphic / Web Design</p>
+                    <Link to='/about' className='introButton'>Continue</Link>
+                </div>
+            </div>
         </>
     );
 }
